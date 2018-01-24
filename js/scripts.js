a493f1decf84dbba46c87697ce501ab52f956322
brunavieirat <41126857858@jpmorgan.sp>
2018-01-24 20:58:13 -0200

    Ok

--- js/scripts.js
+++ js/scripts.js
@@ -2,95 +2,118 @@ var lista = [];
 
 
 
-function atualizarSecao(secao){
+function atualizarSecao(secao) {
 
 
-// criar variavel p guardar o html das notas
+    // criar variavel p guardar o html das notas
 
-    var template="";
+    var template = "";
 
-    
+
     //percorrer a lista de notas e criar o template das notas
 
-   
 
-    for (var i=0; i < lista.length; i++){
-        
 
-        if (lista[i].edit == false){
+    for (var i = 0; i < lista.length; i++) {
 
-    template += '<form class="note" id="form" onclick="editarNota(this.parentElement, ' + i +')">' +
-                '<h1 class="note__title" name="title">' + lista[i].titulo + ' </h1>' + 
-                '<label class="note__body" name="text">' + lista[i].texto + '</label>' +
-                '<button class="note__close" type="button" onclick="removerNota(this.form.parentElement, ' + i + ')">' +
-                    '<i class="fa fa-times" aria-hidden="true"></i>'+
-                    '</button>'+
-                '</form>';
-}
-   
-else {
-    
-   
-    
-    template += '<form class="note" id="form">' +
+
+        if (lista[i].edit == true) {
+
+            //    var teste = lista[i].edit = false;
+
+            template += '<form class="note" id="form">' +
                 '<input class="note__title" name="title" value="' + lista[i].titulo + '" </input>' +
                 '<textarea class="note__body" name="text">' + lista[i].texto + '</textarea>' +
                 '<button class="note__close" type="button" onclick="removerNota(this.form.parentElement, ' + i + ')">' +
-                    '<i class="fa fa-times" aria-hidden="true"></i>'+
-                    '</button>'+
+                '<i class="fa fa-times" aria-hidden="true"></i>' +
+                '</button>' +
+                '<button class="note__control" type="button" onclick="adicionarNota(this.form.title, this.form.text, this.form, this.form.parentElement,' + i + ')">' +
+                'Concluído' +
+                '</button>' +
                 '</form>';
+
+// adicionarNota(this.form.title, this.form.text, this.form, this.form.nextElementSibling, null)
+        }
+
+        else {
+
+            template += '<form class="note" id="form" onclick="editarNota(this.parentElement, ' + i + ')">' +
+                '<h1 class="note__title" name="title">' + lista[i].titulo + ' </h1>' +
+                '<label class="note__body" name="text">' + lista[i].texto + '</label>' +
+                '<button class="note__close" type="button" onclick="removerNota(this.form.parentElement, ' + i + ')">' +
+                '<i class="fa fa-times" aria-hidden="true"></i>' +
+                '</button>' +
+                '</form>';
+
+
+
+        }
+
     }
-}
     // colocar o html de tdo mundo (inner) dentro da secao
 
-    secao.innerHTML= template;
+    secao.innerHTML = template;
+
 }
 
-function removerNota(secao,index){
+function removerNota(secao, index) {
 
-    lista.splice(index,1);
+    lista.splice(index, 1);
 
     atualizarSecao(secao);
 
 }
 
-function editarNota(secao, index){
+function editarNota(secao, index) {
 
-//atributo de edição
+    //atributo de edição   
 
-lista[index].edit=true;
+    lista[index].edit = true;
+    
+    //atualizar tela
 
-
-//atualizar tela
-
-atualizarSecao(secao);
+    atualizarSecao(secao);
 
 }
 
 
 
-function adicionarNota(inputTitulo, textareaTexto, form, secao){
-    // criar uma variável nota 
+function adicionarNota(inputTitulo, textareaTexto, form, secao, index) {
 
-   var nota = {
-        titulo: inputTitulo.value,
-        texto: textareaTexto.value,
+
+
+    if (lista[index]) {
+        //pegar nota
+
+        // atualizar
+        lista[index].titulo = inputTitulo.value;
+        lista[index].texto = textareaTexto.value;
+
+        lista[index].edit = false;
+
+          atualizarSecao(secao);
+    }
+    else {
+
         
-    };
+        var nota = {
+            titulo: inputTitulo.value,
+            texto: textareaTexto.value,
 
-    //adicionar nota na lista
+        };
 
-    lista.push(nota);
+         lista.push(nota);
+        //trocar valores do titulo e texto 
 
-    //atualizar a secao de nota
-atualizarSecao(secao);
+        // lista[index].titulo = inputTitulo.value;
+        // lista[index].texto = textareaTexto.value;
 
-
-console.log(lista);
-
-
-//limpar formulario
-   form.reset();
+        // lista[index].edit = false;
 
 
+        //chamar o atualiza secao
+        atualizarSecao(secao);
+        form.reset();
+
+    }
 }
