bd523012a7c511ac7acf11aab17cae4cacbd7adb
brunavieirat <bruna.vieira.t@hotmail.com>
2018-01-31 12:45:11 -0200

    Refatorada a lista de notas

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,51 @@
 var lista = [];
 
+var listaNotas = {
+    
+        lista: [],
+    
+        adiciona: function(titulo, texto, secao){
+            var nota = {
+                titulo: titulo,
+                texto: texto,
+                edit: false
+                
+            };
+        
+            //adicionar nota na lista
+        
+            this.lista.push(nota);
+
+            atualizarSecao(secao);
+        
+    
+        },
+    
+        remove: function(index, secao){
+            this.lista.splice(index,1);
+
+            atualizarSecao(secao);
+        },
+    
+        edita: function(index, secao){
+    
+            this.lista[index].edit=true;
+            atualizarSecao(secao);
+        },
+    
+        salva: function(index, titulo, texto, secao){
+
+            this.lista[index].titulo = titulo;
+            this.lista[index].texto = texto;
+    
+            this.lista[index].edit = false;
+    
+             atualizarSecao(secao);
+    
+        }
+    
+    
+    }
 
 
 function atualizarSecao(secao) {
@@ -14,33 +60,32 @@ function atualizarSecao(secao) {
 
 
 
-    for (var i = 0; i < lista.length; i++) {
+    for (var i = 0; i < listaNotas.lista.length; i++) {
 
 
-        if (lista[i].edit == true) {
+        if (listaNotas.lista[i].edit) {
 
-            //    var teste = lista[i].edit = false;
+            //    var teste = listaNotas.lista[i].edit = false;
 
             template += '<form class="note" id="form">' +
-                '<input class="note__title" name="title" value="' + lista[i].titulo + '" </input>' +
-                '<textarea class="note__body" name="text">' + lista[i].texto + '</textarea>' +
-                '<button class="note__close" type="button" onclick="removerNota(this.form.parentElement, ' + i + ')">' +
+                '<input class="note__title" name="title" value="' + listaNotas.lista[i].titulo + '" </input>' +
+                '<textarea class="note__body" name="text">' + listaNotas.lista[i].texto + '</textarea>' +
+                '<button class="note__close" type="button" onclick="removerNota(event,' + i + ', this.form.parentElement)">' +
                 '<i class="fa fa-times" aria-hidden="true"></i>' +
                 '</button>' +
-                '<button class="note__control" type="button" onclick="adicionarNota(this.form.title, this.form.text, this.form, this.form.parentElement,' + i + ')">' +
-                'Concluído' +
-                '</button>' +
-                '</form>';
+                '<button class="note__control" type="button" onclick="atualizarNota(this.form.title, this.form.text, this.form, this.form.parentElement, '+ i +')">' +
+                'Salvar' +    
+                '</button>' 
+                '</form>'; 
 
-// adicionarNota(this.form.title, this.form.text, this.form, this.form.nextElementSibling, null)
         }
 
         else {
 
-            template += '<form class="note" id="form" onclick="editarNota(this.parentElement, ' + i + ')">' +
-                '<h1 class="note__title" name="title">' + lista[i].titulo + ' </h1>' +
-                '<label class="note__body" name="text">' + lista[i].texto + '</label>' +
-                '<button class="note__close" type="button" onclick="removerNota(this.form.parentElement, ' + i + ')">' +
+            template += '<form class="note" id="form" onclick="editarNota(' + i + ', this.parentElement, )">' +
+                '<h1 class="note__title" name="title">' + listaNotas.lista[i].titulo + ' </h1>' +
+                '<label class="note__body" name="text">' + listaNotas.lista[i].texto + '</label>' +
+                '<button class="note__close" type="button" onclick="removerNota(event,' + i + ', this.form.parentElement)">' +
                 '<i class="fa fa-times" aria-hidden="true"></i>' +
                 '</button>' +
                 '</form>';
@@ -56,64 +101,44 @@ function atualizarSecao(secao) {
 
 }
 
-function removerNota(secao, index) {
+function removerNota(evento, index, secao) {
+    evento.stopPropagation();
 
-    lista.splice(index, 1);
+    listaNotas.remove(index, secao);
+}
 
-    atualizarSecao(secao);
+function editarNota(index, secao) {
+
+    listaNotas.edita(index, secao);
+    return false;
 
 }
 
-function editarNota(secao, index) {
+function atualizarNota(inputTitulo, textareaTexto, form, secao, index){
+   
+        titulo= inputTitulo.value;
+        texto= textareaTexto.value;
 
-    //atributo de edição   
+   
 
-    lista[index].edit = true;
     
-    //atualizar tela
 
-    atualizarSecao(secao);
-
+    listaNotas.salva(index, titulo, texto, secao);
 }
 
 
+function adicionarNota(inputTitulo, textareaTexto, form, secao) {
 
-function adicionarNota(inputTitulo, textareaTexto, form, secao, index) {
+    
+        var titulo= inputTitulo.value;
+        var texto= textareaTexto.value;
+          
 
-
-
-    if (lista[index]) {
-        //pegar nota
-
-        // atualizar
-        lista[index].titulo = inputTitulo.value;
-        lista[index].texto = textareaTexto.value;
-
-        lista[index].edit = false;
-
-          atualizarSecao(secao);
-    }
-    else {
-
+        listaNotas.adiciona(titulo, texto, secao);
         
-        var nota = {
-            titulo: inputTitulo.value,
-            texto: textareaTexto.value,
 
-        };
+        //  listaNotas.adiciona(titulo, texto, secao);
+         form.reset();
 
-         lista.push(nota);
-        //trocar valores do titulo e texto 
-
-        // lista[index].titulo = inputTitulo.value;
-        // lista[index].texto = textareaTexto.value;
-
-        // lista[index].edit = false;
-
-
-        //chamar o atualiza secao
-        atualizarSecao(secao);
-        form.reset();
-
-    }
+    
 }
