5c00a4135b6aa8eac6984b16b97e3406394282f5
brunavieirat <bruna.vieira.t@hotmail.com>
2018-02-09 16:18:57 -0200

    Reescrevendo componentes

---
+++ src/listaNotas.js
@@ -0,0 +1,65 @@
+import Nota from './nota.js';
+import default from './nota.js';
+
+class ListaNotas{
+
+    constructor(observardor) {
+
+
+        this._lista = [];
+        this._observador = observador; 
+    }
+
+    
+        adiciona (titulo, texto){
+           // Cria um elemento Nota (importado do arquivo nota.js)
+           // é necessário passar os parametros q sao solicitados na classe do outro arquivo (novoTitulo, novoTexto)
+            let  nota = new Nota(titulo, texto),
+
+            //pegar o array de lista e adicionar a nota com parametros passados anteriormente
+            this._lista.push(nota);
+
+            this._observador();                
+                
+            }
+        
+               
+        remove(index){
+            //remove um valor da lista, iniciando pela posicao recebida
+            this.lista.splice(index,1);
+
+        }
+    
+        edita(index){
+            
+            //pega a posição (objeto) e informa q o mesmo esta sendo editado
+            this.lista[index].edit=true;
+            this._observador();  
+        }
+    
+        salva(index, titulo, texto){
+            //recebe a posição, o titulo e o texto que deverão ser alterados (novos valores)
+            //passa os valores digitados para o objeto da lista
+            this.lista[index].titulo = titulo;
+            this.lista[index].texto = texto;
+            
+            //apos editar indica que nao esta mais editando e salva
+            this.lista[index].edit = false;
+
+            this._observador();
+        
+        }
+
+        pega(posicao){
+
+            //pega a posicao do objeto da lista   
+            return this.lista[index];
+        }
+    
+        contaTotal(){
+            return this._lista.length;
+        }
+    
+    };
+
+expor default ListaNotas;
---
+++ src/nota.js
@@ -0,0 +1,18 @@
+class Nota{
+    constructor(novoTitulo, novoTexto){
+
+        this.titulo = novoTitulo;
+        this.texto = novoTexto;
+        this.editando = false;
+
+    }
+
+
+    // titulo= inputTitulo.value;
+    // texto= textareaTexto.value;
+
+
+
+
+}
+export default Notas;
